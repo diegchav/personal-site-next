0804d458088de633bf865029d65cd54d66a6bb98
diegchav <diegchav.me@gmail.com>
2019-10-03 12:11:48 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Diego Chávez | FullStack Web Developer</title>
+      <meta charset="UTF-8" />
+      <link rel='icon' href='/static/favicon.ico' importance='low' />
+    </Head>
+  </div>
+)
+
+export default Home
